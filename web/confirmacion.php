7f8e80e6342d44dcf0648e3ce53c9dd3466c1bf5
marco-novaes98 <43818023+marco-novaes98@users.noreply.github.com>
2021-05-25 18:12:43 +0200

    Add files via upload

---
+++ web/confirmacion.php
@@ -0,0 +1,6 @@
+<br><br>
+<h1 align="center" style="color:green">¡<?php echo $_GET['nombre'];?>, tu reserva ha sido procesada correctamente!</h1>
+<h2 align="center">Dispones de una hora para ocupar tu plaza de parking, tras ese plazo tu reserva será cancelada automáticamente</h2>
+<h2 align="center">Esta reserva solo es válida para el vehículo con matrícula <?php echo $_GET['matricula'];?></h2>
+<h3 align="center">¡Gracias por confiar en la app de Smart Parking!</h3>
+
